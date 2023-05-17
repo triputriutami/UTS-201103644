184297def574f6de547c796da848eb296a32aca4
GitaSoraya <127930678+GitaSoraya@users.noreply.github.com>
2023-05-17 11:47:35 +0700

    Tablayout

---
+++ app/src/main/java/com/TRIPUTRIUTAMI201103644/uts201103644/MainActivity.java
@@ -0,0 +1,57 @@
+package com.TRIPUTRIUTAMI201103644.uts201103644;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TableLayout;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity
+{
+    private MyFragmentPagerAdapter _myFragmentPagerAdapter;
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TabLayout tabLayout1 = (TabLayout) findViewById(R.id.tabLayout1);
+        tabLayout1.addTab(tabLayout1.newTab().setText("E-Commerce"));
+        tabLayout1.addTab(tabLayout1.newTab().setText("Berita"));
+        tabLayout1.addTab(tabLayout1.newTab().setText("Kampus"));
+        tabLayout1.setTabGravity((TabLayout.GRAVITY_FILL);
+
+        final ViewPager viewPager1 = (ViewPager) findViewById(R.id.tabLayout1);
+
+        _myFragmentPagerAdapter = new MyFragmentPagerAdapter(this, getSupportFragmentManager(), tabLayout1.getTabCount();
+        viewPager1.setAdapter(_myFragmentPagerAdapter);
+        viewPager1.setOffscreenPageLimit(3);
+
+        viewPager1.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout1));
+        tabLayout1.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener()
+        {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab)
+            {
+                viewPager1.setCurrentItem(tab.getPosition());
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab)
+            {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/TRIPUTRIUTAMI201103644/uts201103644/MyFragmentPagerAdapter.java
@@ -0,0 +1,9 @@
+package com.TRIPUTRIUTAMI201103644.uts201103644;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+public class MyFragmentPagerAdapter extends FragmentPagerAdapter;
+{
+    public Fragment get
+}
